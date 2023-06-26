4b03eff8ca4ae066f4510d8a06e5d989aa71e1a3
Luiz Felipe <luizfelipecastrovb@gmail.com>
2023-06-26 16:00:55 -0300

    Adicionando o Core da aplicação

---
+++ app/Core/Core.php
@@ -0,0 +1,61 @@
+<?php
+
+class Core
+{
+  private $url;
+  private $controller;
+  private $method = "index";
+  private $params = [];
+  private $user;
+  private $error;
+
+  public function __construct()
+  {
+    $this->user = $_SESSION["user"] ?? null;
+    $this->error = $_SESSION["message_error"] ?? null;
+
+    if (isset($this->error)) {
+      if ($this->error["count"] === 0) {
+        $_SESSION["message_error"]["count"]++;
+      } else {
+        unset($_SESSION["message_error"]);
+      }
+    }
+  }
+
+  public function start($request)
+  {
+    if (isset($request["url"])) {
+      $this->url = explode("/", $request["url"]);
+      $this->controller = ucfirst($this->url[0]) . "Controller";
+      array_shift($this->url);
+
+      if (isset($this->url[0]) && $this->url != "") {
+        $this->method = $this->url[0];
+        array_shift($this->url);
+
+        if (isset($this->url[0]) && $this->url != "") {
+          $this->params = $this->url;
+        }
+      }
+    }
+
+    if ($this->user) {
+      $pagesPermission = ["DashboardController"];
+
+      if (!isset($this->controller) || !in_array($this->controller, $pagesPermission)) {
+        $this->controller = "DashboardController";
+        $this->method = "index";
+      }
+    } else {
+      $pagesPermission = ["LoginController", "RegisterController"];
+
+      if (!isset($this->controller) || !in_array($this->controller, $pagesPermission)) {
+        $this->controller = "LoginController";
+        $this->method = "index";
+      }
+    }
+
+    return call_user_func(array(new $this->controller, $this->method), $this->params);
+  }
+}
